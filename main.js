4ed5e9c23c18d45da30314a2498af118963424fd
XiongAmao <onycat@live.cn>
2017-05-29 17:45:25 +0800

    添加sign-in input框 动态效果

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
-let $tabs = $('.tabs')
-let $form = $('.forms')
-var $formSignIn = $form.find('#sign-in')
-var $formSignUp = $form.find('#sign-up')
-console.log($formSignIn, $formSignUp)
-$tabs.on('click', 'div', function (e) {
-    if (this === document.querySelector('div[data-tab="sign-in"]')) {
-        $tabs.addClass('left').removeClass('right')
-        $formSignUp.removeClass('active')
-        setTimeout(function () {
-            $formSignIn.addClass('active')
-        }, 300);
-    } else if (this === document.querySelector('div[data-tab="sign-up"]')) {
-        $tabs.removeClass('left').addClass('right')
-        $formSignIn.removeClass('active')
-        setTimeout(function () {
-            $formSignUp.addClass('active')
-        }, 300);
-    }
+$(function () {
+    var $tabs = $('.tabs')
+    var $forms = $('.forms')
+    var $formSignIn = $forms.find('#sign-in')
+    var $formSignUp = $forms.find('#sign-up')
+    var $signInInput = $('input[data-input="signIn"]')
+
+    $tabs.on('click', 'div', function (e) {
+        if (this === document.querySelector('div[data-tab="sign-in"]')) {
+            $tabs.addClass('left').removeClass('right')
+            // 存在连续点击bug
+            $formSignUp.removeClass('active')
+            setTimeout(function () {
+                $formSignIn.addClass('active')
+            }, 300);
+        } else if (this === document.querySelector('div[data-tab="sign-up"]')) {
+            $tabs.removeClass('left').addClass('right')
+            $formSignIn.removeClass('active')
+            setTimeout(function () {
+                $formSignUp.addClass('active')
+            }, 300);
+        }
+    })
+
+    $signInInput.on('input change', (e) => {
+        if($signInInput.eq(0).val() !== "" && $signInInput.eq(1).val() !==""){
+            $formSignIn
+                .find('button[data-button="sign-in"]')
+                .prop('disabled', false)
+                .removeClass("disabled")
+        }else{
+            $formSignIn
+                .find('button[data-button="sign-in"]')
+                .prop("disabled", true)
+                .addClass("disabled")
+        }
+        if($signInInput.eq(0).val() !== ""){
+            $signInInput.eq(0).addClass('active')
+        }else{
+            $signInInput.eq(0).removeClass('active')
+        }
+    })
 })

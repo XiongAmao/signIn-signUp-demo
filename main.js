dee7c8c5eff69429fdabdda8668316fb7e250672
XiongAmao <onycat@live.cn>
2017-05-29 11:19:33 +0800

    增加tabs功能：form表单切换

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 let $tabs = $('.tabs')
-$tabs.on('click','div',function(e){
-    if(this === document.querySelector('div[data-tab="sign-in"]')){
+let $form = $('.forms')
+var $formSignIn = $form.find('#sign-in')
+var $formSignUp = $form.find('#sign-up')
+console.log($formSignIn, $formSignUp)
+$tabs.on('click', 'div', function (e) {
+    if (this === document.querySelector('div[data-tab="sign-in"]')) {
         $tabs.addClass('left').removeClass('right')
-    }else if (this === document.querySelector('div[data-tab="sign-up"]')){
+        $formSignUp.removeClass('active')
+        setTimeout(function () {
+            $formSignIn.addClass('active')
+        }, 300);
+    } else if (this === document.querySelector('div[data-tab="sign-up"]')) {
         $tabs.removeClass('left').addClass('right')
+        $formSignIn.removeClass('active')
+        setTimeout(function () {
+            $formSignUp.addClass('active')
+        }, 300);
     }
 })
